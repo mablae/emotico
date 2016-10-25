38cc88a1299c8fb4693340c4b4e2c27853b134ee
pboethig <pboethig@github.com>
2016-10-25 16:30:02 +0200

    add prouction environment

--- data/www/src/ApiBundle/Controller/DefaultController.php
+++ data/www/src/ApiBundle/Controller/DefaultController.php
@@ -4,10 +4,23 @@ namespace ApiBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class DefaultController extends Controller
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Controller\Annotations\QueryParam;
+use FOS\RestBundle\Controller\Annotations\Route;
+use FOS\RestBundle\Controller\Annotations\View;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+use Symfony\Component\HttpFoundation\Request;
+
+class DefaultController extends FOSRestController
 {
     public function indexAction()
     {
         return $this->render('ApiBundle:Default:index.html.twig');
     }
+
+    public function testAction()
+    {
+        return $this->handleView($this->view(array('dasdasd')));
+    }
+
 }
